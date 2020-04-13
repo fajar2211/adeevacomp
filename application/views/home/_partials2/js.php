27a04bbf14825fd3614b333169b818ee9a238428
fajar22 <w4h33d@gmail.com>
2020-04-13 20:28:03 +0700

    adeevacomp

--- application/views/home/_partials2/js.php
+++ application/views/home/_partials2/js.php
@@ -1,4 +1,4 @@
-<!--<script src="<?php echo base_url('html/assets/jquery/jquery.min.js') ?>"></script>-->
+<script src="<?php echo base_url('html/assets/jquery/jquery.min.js') ?>"></script>
 <script src="<?php echo base_url('html/assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
 <!--        <style>
